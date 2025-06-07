e3efc05d766da64dd57d9f46407ce4cafe6f2f29
Ben White <bewhite87@gmail.com>
2025-06-07 10:38:02 +0100

    Refactor DietRestrictionService for dietary preferences
    Updated DietRestrictionService to use DietaryPreferences
    instead of SubscriptionContext. Added a new field for
    checking blocked ingredients and refactored the
    AllowedByPreferences method to improve clarity and
    maintainability.

--- SimmerInterviewTask/Shared/Services/DietRestrictionService.cs
+++ SimmerInterviewTask/Shared/Services/DietRestrictionService.cs
@@ -1,28 +1,37 @@
 ﻿using SimmerInterviewTask.Model;
 using SimmerInterviewTask.Shared.Factories;
 using SimmerInterviewTask.Shared.Services.Abstractions;
+using static SimmerInterviewTask.Model.SubscriptionContext;
 
 namespace SimmerInterviewTask.Shared.Services;
 
 internal sealed class DietRestrictionService(
-    SubscriptionContext subscriptionContext,
-    IMenuItemFilterFactory dietMenuItemStrategyFactory)
+    DietaryPreferences dietaryPreferences,
+    IMenuItemFilterFactory dietMenuItemStrategyFactory,
+    IMenuItemBlockedIngredientsChecker menuItemBlockedIngredientsChecker)
     : IDietRestrictionService
 {
-    private readonly SubscriptionContext _subscriptionContext = subscriptionContext
-        ?? throw new ArgumentNullException(nameof(subscriptionContext));
+    private readonly DietaryPreferences _dietaryPreferences = dietaryPreferences
+        ?? throw new ArgumentNullException(nameof(dietaryPreferences));
 
     private readonly IMenuItemFilterFactory _dietMenuItemStrategyFactory = dietMenuItemStrategyFactory
         ?? throw new ArgumentNullException(nameof(dietMenuItemStrategyFactory));
 
+    private readonly IMenuItemBlockedIngredientsChecker _menuItemBlockedIngredientsChecker = menuItemBlockedIngredientsChecker
+        ?? throw new ArgumentNullException(nameof(menuItemBlockedIngredientsChecker));
+
     public bool AllowedByPreferences(MenuItem menuItem)
     {
         ArgumentNullException.ThrowIfNull(menuItem);
-        ArgumentNullException.ThrowIfNull(_subscriptionContext.DietPreferences);
 
-        Diet? diet = _subscriptionContext.DietPreferences.Diet;
+        Diet? diet = _dietaryPreferences.Diet;
         IMenuItemFilter menuItemFilter = _dietMenuItemStrategyFactory.CreateFor(diet);
 
-        return menuItemFilter.IsAllowed(menuItem);
+        bool isAllowedByDiet = menuItemFilter.IsAllowed(menuItem);
+        bool itemsContainsBlockedIngredients = _menuItemBlockedIngredientsChecker.ContainsBlockedIngredients(
+            _dietaryPreferences.BlockedIngredientIds, 
+            menuItem);
+
+        return isAllowedByDiet && !itemsContainsBlockedIngredients;
     }
 }
