dfada173fdcbb4ba33c5761ba2eb7bc5cdb17bae
Ben White <bewhite87@gmail.com>
2025-06-07 10:01:58 +0100

    Add unit tests for VegetarianMenuItemFilter
    Introduce `VegetarianMenuItemFilterTests` class with three unit tests to validate the behavior of the `VegetarianMenuItemFilter`. Tests cover scenarios for handling null `MenuItem`, and checking the return values for vegetarian and non-vegetarian items. Necessary using directives are also included.

---
+++ Simmer.Tests/Features/Vegetarian/VegetarianMenuItemFilterTests.cs
@@ -0,0 +1,51 @@
+﻿using SimmerInterviewTask.Features.Vegetarian;
+using SimmerInterviewTask.Model;
+using Xunit;
+
+namespace Simmer.Tests.Features.Vegan;
+
+public class VegetarianMenuItemFilterTests
+{
+    [Fact]
+    public void IsAllowed_ShouldThrowArgumentNullException_GivenNullMenuItem()
+    {
+        // Arrange
+        VegetarianMenuItemFilter sut = new();
+        MenuItem? menuItem = null;
+
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => sut.IsAllowed(menuItem!));
+    }
+
+    [Fact]
+    public void IsAllowed_ShouldReturnTrue_GivenAVegetarianMenuItem()
+    {
+        // Arrange
+        VegetarianMenuItemFilter sut = new();
+        MenuItem menuItem = TestDataHelper.CreateMenuItem(
+            isVegan: false,
+            isVegetarian: true);
+
+        // Act
+        bool result = sut.IsAllowed(menuItem);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsAllowed_ShouldReturnFalse_GivenAMenuItemThatIsNotVegetarian()
+    {
+        // Arrange
+        VegetarianMenuItemFilter sut = new();
+        MenuItem menuItem = TestDataHelper.CreateMenuItem(
+            isVegan: false,
+            isVegetarian: false);
+
+        // Act
+        bool result = sut.IsAllowed(menuItem);
+
+        // Assert
+        Assert.False(result);
+    }
+}
