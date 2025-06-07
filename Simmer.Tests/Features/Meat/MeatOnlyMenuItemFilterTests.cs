8dce4a72ef1c67c8014401572e68a47d24f13d6b
Ben White <bewhite87@gmail.com>
2025-06-07 09:48:12 +0100

    Add tests for MeatOnlyMenuItemFilter functionality
    Introduced a new test class `MeatOnlyMenuItemFilterTests` to validate the `IsAllowed` method of the `MeatOnlyMenuItemFilter` class. The tests cover various scenarios, including handling null menu items, confirming true for meat-only items, and ensuring false for vegan or vegetarian main menu items. Additionally, breakfast menu items with vegan or vegetarian options are verified to return true.

---
+++ Simmer.Tests/Features/Meat/MeatOnlyMenuItemFilterTests.cs
@@ -0,0 +1,81 @@
+﻿using SimmerInterviewTask.Features.Meat;
+using SimmerInterviewTask.Model;
+using Xunit;
+
+namespace Simmer.Tests.Features.Meat;
+
+public class MeatOnlyMenuItemFilterTests
+{
+    [Fact]
+    public void IsAllowed_ShouldThrowArgumentNullException_GivenNullMenuItem()
+    {
+        // Arrange
+        MeatOnlyMenuItemFilter sut = new();
+        MenuItem? menuItem = null;
+
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => sut.IsAllowed(menuItem!));
+    }
+
+    [Fact]
+    public void IsAllowed_ShouldReturnTrue_GivenAMenuItemWithMeatOnly()
+    {
+        // Arrange
+        MeatOnlyMenuItemFilter sut = new();
+        MenuItem menuItem = TestDataHelper.CreateMenuItem(
+            isVegan: false,
+            isVegetarian: false);
+
+        // Act
+        bool result = sut.IsAllowed(menuItem);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Theory]
+    [InlineData(true, false)]
+    [InlineData(false, true)]
+    [InlineData(true, true)]
+    public void IsAllowed_ShouldReturnFalse_GivenAMainMenuItemThatIsVeganOrVegtarian(
+        bool isVegan,
+        bool isVegetarian)
+    {
+        // Arrange
+        MeatOnlyMenuItemFilter sut = new();
+        MenuItem menuItem = TestDataHelper.CreateMenuItem(
+            isVegan: isVegan,
+            isVegetarian: isVegetarian,
+            type: MenuItemType.Main
+        );
+
+        // Act
+        bool result = sut.IsAllowed(menuItem);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Theory]
+    [InlineData(true, false)]
+    [InlineData(false, true)]
+    [InlineData(true, true)]
+    public void IsAllowed_ShouldReturnTrue_GivenABreakfastMenuItemThatIsVeganOrVegtarian(
+        bool isVegan,
+        bool isVegetarian)
+    {
+        // Arrange
+        MeatOnlyMenuItemFilter sut = new();
+        MenuItem menuItem = TestDataHelper.CreateMenuItem(
+            isVegan: isVegan,
+            isVegetarian: isVegetarian,
+            type: MenuItemType.Breakfast
+        );
+
+        // Act
+        bool result = sut.IsAllowed(menuItem);
+
+        // Assert
+        Assert.True(result);
+    }
+}
