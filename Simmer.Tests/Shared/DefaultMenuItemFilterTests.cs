53877ee4420718a100e2f75abefe355394d63a49
Ben White <bewhite87@gmail.com>
2025-06-07 09:02:50 +0100

    Add unit tests for DefaultMenuItemFilter
    Introduced the `DefaultMenuItemFilterTests` class in the `Simmer.Tests.Shared` namespace. This class contains two tests for the `IsAllowed` method: one for a valid menu item and another for a null menu item. Added necessary using directives for `SimmerInterviewTask.Core`, `SimmerInterviewTask.Model`, and `Xunit`.

---
+++ Simmer.Tests/Shared/DefaultMenuItemFilterTests.cs
@@ -0,0 +1,44 @@
+﻿using SimmerInterviewTask.Core;
+using SimmerInterviewTask.Model;
+using Xunit;
+
+namespace Simmer.Tests.Shared;
+
+public class DefaultMenuItemFilterTests
+{
+    [Fact]
+    public void IsAllowed_ShouldReturnTrue_GivenAMenuItem()
+    {
+        // Arrange
+        DefaultMenuItemFilter sut = new();
+        MenuItem menuItem = new()
+        {
+            ExcludeFromSuggestions = false,
+            Id = 1,
+            IngredientIds = [1, 2, 3],
+            IsVegan = false,
+            IsVegetarian = false,
+            Type = MenuItemType.Main
+        };
+
+        // Act
+        bool result = sut.IsAllowed(menuItem);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsAllowed_ShouldReturnTrue_GivenNull()
+    {
+        // Arrange
+        DefaultMenuItemFilter sut = new();
+        MenuItem menuItem = null!;
+
+        // Act
+        bool result = sut.IsAllowed(menuItem);
+
+        // Assert
+        Assert.True(result);
+    }
+}
