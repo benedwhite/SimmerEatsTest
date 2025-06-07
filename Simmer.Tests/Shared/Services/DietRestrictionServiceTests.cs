d6c96d84a4ddcbcf8a81638a82b8e01f47344790
Ben White <bewhite87@gmail.com>
2025-06-07 10:10:01 +0100

    Add unit tests for DietRestrictionService
    Introduce `DietRestrictionServiceTests` with a test method
    `AllowedByPreferences_()` to validate the functionality of
    the `AllowedByPreferences` method. The test checks that
    the service correctly allows menu items based on user
    preferences using a set up `SubscriptionContext` and
    `MenuItem`.

---
+++ Simmer.Tests/Shared/Services/DietRestrictionServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using SimmerInterviewTask.Model;
+using SimmerInterviewTask.Shared.Factories;
+using SimmerInterviewTask.Shared.Services;
+using Xunit;
+
+namespace Simmer.Tests.Shared.Services;
+
+public class DietRestrictionServiceTests
+{
+    /**
+     * Due to time constraints, I have included a simple test case below.
+     * However, I have tested the individual filter strategies in their respective test files.
+     */
+
+    [Fact]
+    public void AllowedByPreferences_()
+    {
+        // Arrange
+        SubscriptionContext subscriptionContext = TestDataHelper.CreateSubscriptionContext();
+        IMenuItemFilterFactory menuItemFilterFactory = new MenuItemFilterFactory();
+        MenuItem menuItem = TestDataHelper.CreateMenuItem();
+
+        DietRestrictionService sut = new(subscriptionContext, menuItemFilterFactory);
+
+        // Act
+        bool result = sut.AllowedByPreferences(menuItem);
+
+        // Assert
+        Assert.True(result);
+    }
+}
