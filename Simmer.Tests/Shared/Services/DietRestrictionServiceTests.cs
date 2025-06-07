bbd3da10c92048dda6bbe9c18c2e951c29d5a19c
Ben White <bewhite87@gmail.com>
2025-06-07 11:20:27 +0100

    Add unit tests for DietRestrictionService constructor
    Updated `DietRestrictionServiceTests.cs` to include new unit tests that check for `ArgumentNullException` when null arguments are passed to the `DietRestrictionService` constructor and its `AllowedByPreferences` method. Removed references to `SubscriptionContext.DietaryPreferences` to simplify test setup and improve clarity by directly using `DietaryPreferences` from `TestDataHelper`.

--- Simmer.Tests/Shared/Services/DietRestrictionServiceTests.cs
+++ Simmer.Tests/Shared/Services/DietRestrictionServiceTests.cs
@@ -2,23 +2,79 @@
 using SimmerInterviewTask.Shared.Factories;
 using SimmerInterviewTask.Shared.Services;
 using Xunit;
+using static SimmerInterviewTask.Model.SubscriptionContext;
 
 namespace Simmer.Tests.Shared.Services;
 
 public class DietRestrictionServiceTests
 {
     [Fact]
+    public void Constructor_ShouldThrowArgumentNullException_GivenNullDietPreferences()
+    {
+        // Arrange
+        DietaryPreferences dietaryPreferences = null!;
+
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() =>
+            new DietRestrictionService(
+                dietaryPreferences,
+                CreateMenuItemFilterFactory(),
+                CreateMenuItemBlockedIngredientsChecker()));
+    }
+
+    [Fact]
+    public void Constructor_ShouldThrowArgumentNullException_GivenNullMenuItemFilterFactory()
+    {
+        // Arrange
+        MenuItemFilterFactory menuItemFilterFactory = null!;
+
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() =>
+            new DietRestrictionService(
+                TestDataHelper.CreateDietaryPreferences(),
+                menuItemFilterFactory,
+                CreateMenuItemBlockedIngredientsChecker()));
+    }
+
+    [Fact]
+    public void Constructor_ShouldThrowArgumentNullException_GivenNullMenuItemBlockedIngredientsChecker()
+    {
+        // Arrange
+        MenuItemBlockedIngredientsChecker menuItemBlockedIngredientsChecker = null!;
+
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() =>
+            new DietRestrictionService(
+                TestDataHelper.CreateDietaryPreferences(),
+                CreateMenuItemFilterFactory(),
+                menuItemBlockedIngredientsChecker));
+    }
+
+    [Fact]
+    public void AllowedByPreferences_ShouldThrowArgumentNullException_GivenNullMenuItem()
+    {
+        // Arrange
+        MenuItem menuItem = null!;
+        DietRestrictionService sut = new(
+            TestDataHelper.CreateDietaryPreferences(),
+            CreateMenuItemFilterFactory(),
+            CreateMenuItemBlockedIngredientsChecker());
+
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => sut.AllowedByPreferences(menuItem));
+    }
+
+    [Fact]
     public void AllowedByPreferences_ReturnsTrue_WhenDietIsEverythingAndNoBlockedIngredients()
     {
         // Arrange
-        SubscriptionContext.DietaryPreferences dietaryPreferences = TestDataHelper.CreateDietaryPreferences(
+        DietaryPreferences dietaryPreferences = TestDataHelper.CreateDietaryPreferences(
             Diet.Everything,
             blockedIngredientIds: []);
-        SubscriptionContext subscriptionContext = TestDataHelper.CreateSubscriptionContext(dietaryPreferences);
         MenuItem menuItem = TestDataHelper.CreateMenuItem();
 
         DietRestrictionService sut = new(
-            subscriptionContext.DietPreferences,
+            dietaryPreferences,
             CreateMenuItemFilterFactory(),
             CreateMenuItemBlockedIngredientsChecker());
 
@@ -33,14 +89,13 @@ public class DietRestrictionServiceTests
     public void AllowedByPreferences_ReturnsFalse_WhenDietIsMeatOnlyAndMenuItemIsVegan()
     {
         // Arrange
-        SubscriptionContext.DietaryPreferences dietaryPreferences = TestDataHelper.CreateDietaryPreferences(
+        DietaryPreferences dietaryPreferences = TestDataHelper.CreateDietaryPreferences(
             Diet.MeatOnly,
             blockedIngredientIds: []);
-        SubscriptionContext subscriptionContext = TestDataHelper.CreateSubscriptionContext(dietaryPreferences);
         MenuItem menuItem = TestDataHelper.CreateMenuItem(isVegan: true);
 
         DietRestrictionService sut = new(
-            subscriptionContext.DietPreferences,
+            dietaryPreferences,
             CreateMenuItemFilterFactory(),
             CreateMenuItemBlockedIngredientsChecker());
 
@@ -55,14 +110,13 @@ public class DietRestrictionServiceTests
     public void AllowedByPreferences_ReturnsFalse_WhenDietIsEverythingAndContainsBlockedIngredients()
     {
         // Arrange
-        SubscriptionContext.DietaryPreferences dietaryPreferences = TestDataHelper.CreateDietaryPreferences(
+        DietaryPreferences dietaryPreferences = TestDataHelper.CreateDietaryPreferences(
             Diet.Everything,
             blockedIngredientIds: [1]);
-        SubscriptionContext subscriptionContext = TestDataHelper.CreateSubscriptionContext(dietaryPreferences);
         MenuItem menuItem = TestDataHelper.CreateMenuItem(ingredientIds: [1]);
 
         DietRestrictionService sut = new(
-            subscriptionContext.DietPreferences,
+            dietaryPreferences,
             CreateMenuItemFilterFactory(),
             CreateMenuItemBlockedIngredientsChecker());
 
@@ -77,14 +131,13 @@ public class DietRestrictionServiceTests
     public void AllowedByPreferences_ReturnsTrue_WhenDietIsEverythingAndDoesNotContainBlockedIngredients()
     {
         // Arrange
-        SubscriptionContext.DietaryPreferences dietaryPreferences = TestDataHelper.CreateDietaryPreferences(
+        DietaryPreferences dietaryPreferences = TestDataHelper.CreateDietaryPreferences(
             Diet.Everything,
             blockedIngredientIds: [1]);
-        SubscriptionContext subscriptionContext = TestDataHelper.CreateSubscriptionContext(dietaryPreferences);
         MenuItem menuItem = TestDataHelper.CreateMenuItem(ingredientIds: [2]);
 
         DietRestrictionService sut = new(
-            subscriptionContext.DietPreferences,
+            dietaryPreferences,
             CreateMenuItemFilterFactory(),
             CreateMenuItemBlockedIngredientsChecker());
 
